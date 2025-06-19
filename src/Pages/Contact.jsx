55ae02d57e015fd2c24a9e5311922c9b6e6bdd16
Murali... <msmurali2005@gmail.com>
2025-06-19 10:42:06 +0530

    using-server

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -96,7 +96,7 @@ const Contact = () => {
     }
 
     try {
-      const res = await fetch('https://smtp-server-s4rq.onrender.com/api/contact', {
+      const res = await fetch('https://murali-portfolio.onrender.com/api/contact', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData)
