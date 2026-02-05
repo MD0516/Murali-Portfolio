070577a7bfc58e8c4c828f5c8ae9724230e0c58b
Murali... <msmurali2005@gmail.com>
2026-02-05 12:38:02 +0530

    fix: cookie issue

--- src/Api/axios.jsx
+++ src/Api/axios.jsx
@@ -3,7 +3,7 @@ import axios from "axios"
 const API_URL = import.meta.env.VITE_API_URL
 
 export const api = axios.create({
-    baseURL: API_URL,
+    baseURL: '',
     headers: {
         "Content-Type": "application/json",
         "Accept": "application/json",
